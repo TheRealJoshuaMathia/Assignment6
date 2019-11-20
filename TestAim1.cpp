1a57424968ef6d8462c55fbb3da9699064f59ed5
parkerg44 <56697192+parkerg44@users.noreply.github.com>
2019-11-20 15:20:59 -0800

    Create TestAim1.cpp

---
+++ TestAim1.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <vector>
+#include <cstddef>
+#include <cstdlib>
+
+
+
+
+//using namespace std::chrono;
+using std::fstream;
+using std::ifstream;
+using std::ofstream;
+using std::istream;
+using std::ostream;
+using namespace std;
+
+
+
+
+int main(void)
+{
+	//1. read in data line by line one word per line) load into vector of strings vector<string> DataArray
+
+
+	//2. Read in data from queries and store in vector of strings vector<string> QueryArray
+
+	//3. hash table (HT) implementations. Let 
+	//us denote the corresponding objects as "ChainingHT", "LinearProbingHT"
+	//		and "QuadraticProbingHT
+	//need to create HT class and use polymorpthism for ChainingHT ..
+	//have one class with all implemetations
+
+
+	//4.  "int hash( const string & key, int tableSize )" for implementing all we will keep this hash function as-is, and just 
+	//experiment by varying the collision resolution implementations, including
+	//chaining, linear probingand quadratic probing.
+
+
+
+	return 0;
+}
+
