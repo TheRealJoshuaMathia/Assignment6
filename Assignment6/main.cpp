e5f1b9d921fc39c1b67e424d512b20ea4d46a740
TheRealJoshuaMathia <joshuamathiaprog@yahoo.com>
2019-11-24 16:34:31 -0800

    Initial Commit

---
+++ Assignment6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment6
+//
+//  Created by Joshua Mathia on 11/24/19.
+//  Copyright © 2019 Joshua Mathia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
